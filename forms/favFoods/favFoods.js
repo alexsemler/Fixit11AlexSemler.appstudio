13c2216529351e50ac3ceef2920919e2f836cd70
alexsemler <alex.semler03@gmail.com>
2019-10-30 15:15:02 -0500

    V2

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,18 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+btnNextForm.onclick=function(){
+  ChangeForm(dessertVoting)
+}
+
+
+favFoods.onshow=function(){
+    for (i = 0; i <= goodFoods.length - 1; i++) 
+        listFood.addItem(goodFoods[i])
+}
+
+listFood.onclick=function(choice){
+  if (typeof(choice) == "object")   // user clicked the control
+    return
+    
+  lblOutput.value = ("The choice is " + goodFoods[choice] + ".")
+}
